324e4fc75347d024e5ebc4cc9ec3127c6be9ac59
cmbak <chikmii@gmail.com>
2025-01-05 19:01:04 +0000

    Assign things to movement script and add event listeners to buttons

---
+++ Assets/Scripts/ConnectUI.cs
@@ -0,0 +1,17 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectUI : MonoBehaviour
+{
+    // Network! https://www.youtube.com/watch?v=2OLUdPkkQPI
+    public void ConnectHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void ConnectClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
